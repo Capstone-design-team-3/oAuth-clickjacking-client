eded96a3cc9cc7c647a2317cb196ea7dd56e6573
701b <701b.vn14a@gmail.com>
2021-12-02 20:44:37 +0900

    fix about 4 grant type

--- routes/login.js
+++ routes/login.js
@@ -3,24 +3,23 @@ const router = express.Router()
 const request = require('request')
 const url = require('url')
 
-/* GET users listing. */
 router.get('/', (req, res) => {
     res.render('login.html')
 })
 
-router.get('/oauth-login', (req, res) => {
+router.get('/oauth-login-grant', (req, res) => {
     res.redirect(url.format({
         pathname: 'http://localhost:9000/oauth/authorize',
         query: {
             'response_type': 'code',
             'client_id': 'client',
-            'redirect_uri': 'http://localhost:3005/login/oauth',
+            'redirect_uri': 'http://localhost:3005/login/oauth-grant',
             'scope': 'read'
         }
     }))
 })
 
-router.get('/oauth', (req, res) => {
+router.get('/oauth-grant', (req, res) => {
     const code = req.query['code']
     const options = {
         url: 'http://client:secret@localhost:9000/oauth/token',
@@ -29,36 +28,84 @@ router.get('/oauth', (req, res) => {
             'grant_type': 'authorization_code',
             'code': code,
             'scope': 'read',
-            'redirect_uri': 'http://localhost:3005/login/oauth'
+            'redirect_uri': 'http://localhost:3005/login/oauth-grant'
         }
     }
     
-    request(options, (error, response, body) => {
+    function callback(error, response, body) {
         if (!error && response.statusCode === 200) {
             const token = JSON.parse(body)['access_token']
-            const options = {
-                url: 'http://localhost:9090/account',
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                }
-            }
-        
-            request(options, (error, response, body) => {
-                const accountInfo = JSON.parse(body)
     
-                console.log(accountInfo)
-            
-                res.send('Get Account!')
-            })
+            console.log(token)
+            res.send(token)
         }
-    })
+    }
+    
+    request(options, callback)
 })
 
-router.get('/logout', (req, res) => {
+router.get('/oauth-login-implicit', (req, res) => {
     res.redirect(url.format({
-        pathname: 'http://localhost:3005'
+        pathname: 'http://localhost:9000/oauth/authorize',
+        query: {
+            'response_type': 'token',
+            'client_id': 'client',
+            'redirect_uri': 'http://localhost:3005/login/oauth-implicit',
+            'scope': 'read'
+        }
     }))
+})
+
+router.get('/oauth-implicit', (req, res) => {
+    res.redirect(url.format({
+        pathname: 'http://localhost:3000/'
+    }))
+})
+
+router.get('/oauth-login-password', (req, res) => {
+    const options = {
+        url: 'http://client:secret@localhost:9000/oauth/token',
+        method: 'POST',
+        form: {
+            'grant_type': 'password',
+            'scope': 'read',
+            'user': 'foo',
+            'password': 'foo'
+        }
+    }
+    
+    function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const token = JSON.parse(body)['access_token']
+    
+            console.log(token)
+            res.send(token)
+        }
+    }
+    
+    request(options, callback)
+})
+
+router.get('/oauth-login-client', (req, res) => {
+    const options = {
+        url: 'http://client:secret@localhost:9000/oauth/token',
+        method: 'POST',
+        form: {
+            'grant_type': 'client_credentials',
+            'scope': 'read',
+        }
+    }
+    
+    function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const token = JSON.parse(body)['access_token']
+    
+            console.log(token)
+            res.send(token)
+        }
+    }
+    
+    request(options, callback)
 })
 
 module.exports = router
